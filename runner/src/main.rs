786a86d135118b724d7e16d8fdfa29b72d34697d
Philipp Oppermann <dev@phil-opp.com>
2022-10-02 20:48:58 +0200

    Update to latest bootloader version

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -3,13 +3,17 @@ use std::process::Command;
 use anyhow::Result;
 
 fn main() -> Result<()> {
-    let boot_partition_path = AsRef::as_ref("target/boot.fat");
-    bootloader::create_boot_partition(
-        AsRef::as_ref(env!("CARGO_BIN_FILE_TEST_KERNEL_test-kernel")),
-        boot_partition_path,
-    )?;
     let out_gpt_path = &AsRef::as_ref("target/uefi.img");
-    bootloader::create_uefi_disk_image(boot_partition_path, out_gpt_path)?;
+    bootloader::UefiBoot::new(AsRef::as_ref(env!(
+        "CARGO_BIN_FILE_TEST_KERNEL_test-kernel"
+    )))
+    .create_disk_image(out_gpt_path)?;
+
+    let out_bios_path = &AsRef::as_ref("target/bios.img");
+    bootloader::BiosBoot::new(AsRef::as_ref(env!(
+        "CARGO_BIN_FILE_TEST_KERNEL_test-kernel"
+    )))
+    .create_disk_image(out_bios_path)?;
 
     let mut cmd = Command::new("qemu-system-x86_64");
     cmd.arg("-bios").arg(ovmf_prebuilt::ovmf_pure_efi());
